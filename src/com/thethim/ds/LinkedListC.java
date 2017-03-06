65027cbfbd606168c3f956579d153534e02838ad
Avinash Reddy <beavinash15@gmail.com>
2017-03-06 14:07:07 -0500

    Small Changes

---
+++ src/com/thethim/ds/LinkedListC.java
@@ -0,0 +1,16 @@
+package com.thethim.ds;
+
+public class LinkedListC {
+	
+	Node head;
+
+	public LinkedListC() {
+		// TODO Auto-generated constructor stub
+		head = null;
+	}
+	
+	public void add(Object value) {
+		Node newNode = new Node(value, null);
+	}
+
+}
---
+++ src/com/thethim/ds/Node.java
@@ -0,0 +1,15 @@
+package com.thethim.ds;
+
+public class Node {
+	
+	Object value;
+	Node next;
+
+	public Node(Object value, Node next) {
+		// TODO Auto-generated constructor stub
+		
+		this.value = value;
+		this.next = next;
+	}
+
+}
